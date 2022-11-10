5440a0201b82cb896aec57206ea3e524993e21d4
Jonathan Sandjong <jonathan.sandjong83@frenship.net>
2022-11-09 21:28:19 -0600

    Change max speed constant

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -33,7 +33,7 @@ public final class Constants {
     public static final double kWheelBase = 1;
 
     public static final double kDeadband = 0.05;
-    public static final double kMaxSpeed = 3;
+    public static final double kMaxSpeed = 0.5;
 
     public static final int kConfigTimeout = 100;
   }
